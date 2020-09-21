2074461504279b79fd8e7c50555835a89ab7b3f5
cooliotonyio <tony.ytzhao@gmail.com>
2020-09-21 16:00:20 -0700

    audio extraction

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Download training set from google drive and unzip in root
 
 Create conda environnment with `conda env create -f environment.yml`
 
-Activate conda enviroment and download videos with `conda activate video-mem && python setup.py`
+### Download videos and extract audio
+
+Activate conda environment with `conda activate video-mem`
+Run setup with `python setup.py` (downloads videos and extracts audio)
+Audio extraction requires command line access to [FFmpeg](https://ffmpeg.org/)
 
 ## Running a Jupyter Server
 
@@ -14,10 +18,11 @@ Activate conda enviroment and download videos with `conda activate video-mem && 
 
 ## File structure
 
-```
+```bash
 - training_set/         #Data belongs here
   - Features/
   - Videos/             #Created by setup.py
+  - Audio/              #Created by setup.py
   - video_urls.csv
   - ...
 - setup.py              #Downloads videos
--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import requests
 import os
+import subprocess
 import tqdm
 import pandas as pd
+
+VIDEO_DIR = "training_set/Videos"
+AUDIO_DIR = "training_set/Audio"
 
 
 def download_video(name, url):
@@ -12,17 +16,35 @@ def download_video(name, url):
                 f.write(chunk)
 
 
+def extract_audio(video_path, filename):
+    subprocess.run(["ffmpeg", "-i", video_path, "-vn", filename],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
 if __name__ == "__main__":
     if os.path.exists("training_set/video_urls.csv"):
+
+        # Download videos
         videos = pd.read_csv(
             "training_set/video_urls.csv").set_index("video_id")
-        if not os.path.exists("training_set/Videos"):
-            os.mkdir("training_set/Videos")
+        if not os.path.exists(VIDEO_DIR):
+            os.mkdir(VIDEO_DIR)
         for v, b in tqdm.tqdm(list(videos.iterrows()), desc="Downloading videos"):
-            name = f"training_set/Videos/{v}.mp4"
+            name = f"{VIDEO_DIR}/{v}.mp4"
             if not os.path.exists(name):
                 download_video(name, b["video_url"])
 
+        # Extract audio
+        if not os.path.exists(AUDIO_DIR):
+            os.mkdir(AUDIO_DIR)
+        video_filenames = [
+            f for f in os.listdir(VIDEO_DIR) if f[-4:] == ".mp4"]
+        for video_filename in tqdm.tqdm(video_filenames, desc="Extracting audio"):
+            video_path = f"{VIDEO_DIR}/{video_filename}"
+            filename = f"{AUDIO_DIR}/{video_filename[:-4]}.mp3"
+            if not os.path.exists(filename):
+                extract_audio(video_path, filename)
+
     else:
         print("Could not find training_set!")
-    print("Finished downloading videos")
+    print("Finished setup")
