4ee482c1f5aa8060137df8ddae0ea3609a16e9df
cooliotonyio <tony.ytzhao@gmail.com>
2020-11-04 15:41:12 -0800

    cleaned up glove extraction

--- features/config.py
+++ features/config.py
@@ -13,3 +13,8 @@ EMOTIONS = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
 # Video
 C3D_FEATURE_DIR = "training_set/Features/C3D"
+
+# Text
+GLOVE_WORD_EMBEDDINGS_PATH = "data/glove.6B/glove.6B.300d.txt"
+GLOVE_WORD_EMBEDDING_DIM = 300
+GLOVE_FEATURE_DIR = "training_set/Features/GloVe"
