2e231105ab55b7cc82a0cfa5d7f430f0065e746c
cooliotonyio <tony.ytzhao@gmail.com>
2020-11-09 21:33:27 -0800

    some cleanup

--- features/config.py
+++ features/config.py
@@ -1,20 +1,26 @@
+DATASET = "training_set"
+
 # Images (Frames)
-FRAMES_DIR = "training/N-Frames"
-RESNET152_FEATURE_DIR = "training_set/Features/ResNet152"
-HOG_FEATURE_DIR = "training_set/Features/HOG"
-LBP_FEATURE_DIR = "training_set/Features/LBP"
+FRAMES_DIR = f"{DATASET}/Frames"
+RESNET152_FEATURE_DIR = f"{DATASET}/Features/ResNet152"
+HOG_FEATURE_DIR = f"{DATASET}/Features/HOG"
+LBP_FEATURE_DIR = f"{DATASET}/Features/LBP"
 
 # Audio
-VGGISH_FEATURE_DIR = "training_set/Features/VGGish"
+VGGISH_FEATURE_DIR = f"{DATASET}/Features/VGGish"
 
 # Emotion
-EMOTION_FEATURE_DIR = "training_set/Features/Emotion"
+EMOTION_FEATURE_DIR = f"{DATASET}/Features/Emotion"
 EMOTIONS = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
 # Video
-C3D_FEATURE_DIR = "training_set/Features/C3D"
+C3D_FEATURE_DIR = f"{DATASET}/Features/C3D"
 
 # Text
 GLOVE_WORD_EMBEDDINGS_PATH = "data/glove.6B/glove.6B.300d.txt"
 GLOVE_WORD_EMBEDDING_DIM = 300
-GLOVE_FEATURE_DIR = "training_set/Features/GloVe"
+GLOVE_FEATURE_DIR = f"{DATASET}/Features/GloVe"
+
+def set_dataset(constant, dataset):
+    '''Given constant is a string such as 'training_set/Video', returns '{dataset}/Video'. '''
+    return f"{dataset}/{constant.split('/')[0]}"
