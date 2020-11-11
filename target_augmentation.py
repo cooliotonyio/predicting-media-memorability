1186933a8ba6a347b9474cb774a705687108c10d
cooliotonyio <tony.ytzhao@gmail.com>
2020-11-11 12:29:27 -0800

    addressed strange inconsistencies in dev_annotations

--- target_augmentation.py
+++ target_augmentation.py
@@ -1,10 +1,27 @@
 import pandas as pd
 import numpy as np
 
-def add_position_delta(annotations): 
-    annotations["t"] = annotations["video_position_second"] - annotations["video_position_first"]
 
-def calculate_alpha(annotations, m_T, T = 75):
+def add_position_delta(annotations):
+    t = (annotations["video_position_second"] -
+         annotations["video_position_first"]).to_numpy()
+
+    # Some annotations indicate second viewing occured before first
+    # which should be impossible, so we'll just flip negative values
+    t = np.abs(t)
+
+    # Some annotations indicate first_video and second_video are the same (t=0)
+    # which should be impossible, so we'll just assume t=1 for those
+    t[t == 0] = 1
+
+    # Some annotations are missing values so that t is NaN, replace with mean
+    t = np.nan_to_num(t, nan=np.mean(t[np.logical_not(np.isnan(t))]))
+
+    annotations["t"] = t
+    return annotations
+
+
+def calculate_alpha(annotations, m_T, T=75):
     numerator = 0
     denominator = 0
     for video_id, video in annotations.groupby("video_id"):
@@ -13,12 +30,14 @@ def calculate_alpha(annotations, m_T, T = 75):
         n = len(video)
         m = m_T[video_id]
         for j in range(n):
-            numerator += (np.log(t[j] / T) * (x[j] - m)) / n
-            denominator += (np.log(t[j] / T) ** 2) / n
-
+            num = (np.log(t[j] / T) * (x[j] - m)) / n
+            den = (np.log(t[j] / T) ** 2) / n
+            numerator += num
+            denominator += den
     return numerator / denominator
 
-def calculate_memorability(annotations, alpha, T = 75):
+
+def calculate_memorability(annotations, alpha, T=75):
     m_T = {}
     for video_id, video in annotations.groupby("video_id"):
         t = video["t"].to_numpy()
@@ -30,9 +49,10 @@ def calculate_memorability(annotations, alpha, T = 75):
         m_T[video_id] = m / n
     return m_T
 
-def calculate_alpha_and_memorability(annotations, T, num_iterations = 10):
-    m_T = {video_id: 1 for video_id in annotations["video_id"]}
+
+def calculate_alpha_and_memorability(annotations, T, num_iterations=10):
+    m_T = {video_id: 1 for video_id in annotations["video_id"].unique()}
     for i in range(num_iterations):
-        alpha = calculate_alpha(annotations, m_T, T = T)
-        m_T = calculate_memorability(annotations, alpha, T = T)
+        alpha = calculate_alpha(annotations, m_T, T=T)
+        m_T = calculate_memorability(annotations, alpha, T=T)
     return alpha, pd.Series(m_T)
